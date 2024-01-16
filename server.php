7c755581ad0fefcba5349853c770f6abbd5b9eaf
federicaarosio <arosio.fede@gmail.com>
2024-01-16 16:14:15 +0100

    added server.php

---
+++ server.php
@@ -0,0 +1,11 @@
+<?php
+
+// Pagina a cui arrivo via api. Espone il file json
+
+
+// recupero i dati
+$discs = file_get_contents('./json/dischi.json');
+//avviso il browser che riceverà un .json
+header('Content-Type: application/json');
+//no devo fare encode, il file è già json
+echo $discs;
