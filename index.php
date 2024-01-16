3f184831968a3e1c9553fb5ad79b6798c86283ab
federicaarosio <arosio.fede@gmail.com>
2024-01-16 15:44:57 +0100

    added index.php, added dischi.json

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php dischi json</title>
+
+    <!-- axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+
+<body>
+    <div id="app">
+
+
+    </div>
+
+
+</body>
+
+</html>
